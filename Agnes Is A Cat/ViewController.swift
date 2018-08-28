5c5fb5d5a2a1efa9db400f9b83b0ec1cc37bc2a3
szzhumy <szzhumy@users.noreply.github.com>
2018-08-28 16:20:23 -0700

    version 1.0

---
+++ Agnes Is A Cat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Agnes Is A Cat
+//
+//  Created by Haiying Zhu on 8/28/18.
+//  Copyright © 2018 Haiying Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
